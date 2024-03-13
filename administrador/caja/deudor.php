0ca8b9a479053ac23fe92349bec73828dc8bc145
Fabian Nogueroles <laboratorio.fabian@gmail.com>
2024-03-13 16:36:33 -0300

    13/03/2024

--- administrador/caja/deudor.php
+++ administrador/caja/deudor.php
@@ -30,7 +30,7 @@
 
 <body>
 
-    <h3>Aca guarda los datos en la tabla caja</h3>
+
 
 
     <?php
@@ -104,7 +104,7 @@
     }
 
     //exit();
-
+    /*
 
     $sql = "INSERT INTO caja (
                               fecha_dep,
@@ -141,7 +141,7 @@
         echo "Error al insertar datos: " . mysqli_error($conn);
     }
 
-
+*/
 
     ?>
     <div>
@@ -177,26 +177,26 @@
                     pago_en_efect, 
                     pago_en_voucher, 
                     saldo_del_movil 
-            FROM caja WHERE 1";
+            FROM caja WHERE movil = '$movil'";
 
             $result = $con->query($sql_2);
             while ($row = $result->fetch_assoc()) {
 
             ?>
                 <tr>
-                    <td><?php echo $row['id']; ?></td>
-                    <td><?php echo $row['fecha_dep'] ?></td>
-                    <td><?php echo $row['fecha_actual'] ?></td>
-                    <td><?php echo $row['movil'] ?></td>
-                    <td><?php echo $row['semana'] ?></td>
-                    <td><?php echo $row['semana_actual'] ?></td>
-                    <td><?php echo $row['debe_de_semanas'] ?></td>
-                    <td><?php echo $row['queda_al_movil'] ?></td>
-                    <td><?php echo $row['queda_a_base']; ?></td>
-                    <td><?php echo $row['deuda_movil']; ?></td>
-                    <td><?php echo $row['pago_en_efect']; ?></td>
-                    <td><?php echo $pago_en_papel = $row['pago_en_voucher']; ?></td>
-                    <td><?php echo $row['saldo_del_movil']; ?></td>
+                    <td><?php echo $nu_id = $row['id']; ?></td>
+                    <td><?php echo $nu_fecha_dep = $row['fecha_dep'] ?></td>
+                    <td><?php echo $nu_fecha_actual = $row['fecha_actual'] ?></td>
+                    <td><?php echo $nu_movil = $row['movil'] ?></td>
+                    <td><?php echo $nu_semana = $row['semana'] ?></td>
+                    <td><?php echo $nu_semana_actual = $row['semana_actual'] ?></td>
+                    <td><?php echo $nu_debe_de_semanas = $row['debe_de_semanas'] ?></td>
+                    <td><?php echo $nu_queda_al_movil = $row['queda_al_movil'] ?></td>
+                    <td><?php echo $nu_queda_a_base = $row['queda_a_base']; ?></td>
+                    <td><?php echo $nu_deuda_movil = $row['deuda_movil']; ?></td>
+                    <td><?php echo $nu_pago_en_efect = $row['pago_en_efect']; ?></td>
+                    <td><?php echo $nu_pago_en_papel = $row['pago_en_voucher']; ?></td>
+                    <td><?php echo $nu_saldo_del_movil = $row['saldo_del_movil']; ?></td>
                 </tr>
             <?php
             }
@@ -205,43 +205,97 @@
         </table>
 
         <?php
-        $leer_para_des = "SELECT * FROM caja WHERE $movil";
-        $es_igual = $con->query($leer_para_des);
-        while ($prim = $es_igual->fetch_assoc()) {
-        ?>
-            <div>
-                <tr>
-                    <td><?php echo $prim['movil'] ?></td>
-                    <td><?php echo $debe_de_s = $prim['debe_de_semanas'] ?></td>
-                    <td><?php echo $queda_sin_semanas = $prim['queda_al_movil'] ?></td>
-                    <td>
-                        <?php echo $semana_menos_voucher = $queda_sin_semanas - $debe_de_s;
-                        $actualiza_saldo = "";
-                        ?>
-                    </td>
-                </tr>
-            </div>
-        <?php
+        echo $nu_id;
+        echo "<br>";
+        echo $nu_fecha_dep;
+        echo "<br>";
+        echo $nu_fecha_actual;
+        echo "<br>";
+        echo $nu_movil;                     //movil
+        echo "<br>";
+        echo $nu_semana;                    //semana del voucher
+        echo "<br>";
+        echo $nu_semana_actual;             //semana actual
+        echo "<br>";
+        echo $nu_debe_de_semanas;           //deuda acumulada de semanas
+        echo "<br>";
+        echo $nu_queda_al_movil;            //plata que le queda en FT al movil
+        echo "<br>";
+        echo $nu_queda_a_base;              //porcentaje que le queda a la base
+        echo "<br>";
+        echo $nu_deuda_movil;               //lo que se le debe depositar al movil  
+        echo "<br>";
+        echo $nu_pago_en_efect;             //plata que trajo en el caso de que la hubiera
+        echo "<br>";
+        echo $nu_pago_en_papel;             //suma de todo lo que pago con voucher
+        echo "<br>";
+        echo $nu_saldo_del_movil;           //plata que le resta al movil
+        echo "<br>";
+        if ($nu_deuda_movil > $nu_debe_de_semanas) {
+            echo "Puede pagar semanas adeudadas";
+            echo "<br>";
+            echo $semanas_que_puede_pagar = $nu_semana_actual - $nu_semana;
+            echo "<br>";
+            echo "Se le descuentan: " . $semana_que_puede = $semanas_que_puede_pagar * $abono_semanal . " de semanas adeudadas";
+            echo "<br>";
+            echo "Le Quedan:" . $le_quedan = $nu_deuda_movil - $semana_que_puede;
+        } else {
+            echo "No le alcanza:";
         }
-
         ?>
-        <div class="grid">
-            <div>
-                <ul>
-                    <li>Saldo en voucher: <?php
-                                            // echo "$" . $pago_en_papel;
-                                            ?></li>
-                    <li>Saldo en Efectivo: </li>
-                    <li>Debe de semanas: <?php //echo $debe_semanas 
-                                            ?> </li>
-                </ul>
-            </div>
-            <div>
-                <ul>
-                    <li></li>
-                    <li></li>
-                </ul>
-            </div>
+    </div>
+    <?php
+/*
+    $leer_para_des = "SELECT * FROM caja WHERE $movil";
+    $es_igual = $con->query($leer_para_des);
+    while ($prim = $es_igual->fetch_assoc()) {
+    ?>
+        <div>
+            <tr>
+                <td><?php echo "Movil: " . $prim['movil'] ?></td>
+                <br>
+                <td><?php echo "Debe de semanas: " . $debe_de_s = $prim['debe_de_semanas'] ?></td>
+                <br>
+                <td><?php echo "Quedan al movil" . $queda_sin_semanas = $prim['queda_al_movil'] ?></td>
+                <td>
+                    <?php echo "Semana menos voucher: " . $semana_menos_voucher = $queda_sin_semanas - $debe_de_s;
+                    $actualiza_saldo = "";
+                    ?>
+                </td>
+            </tr>
+        </div>
+    <?php
+    }
+
+
+
+    $sql_3 = "UPDATE caja SET deuda_movil = '$semana_menos_voucher' +1 WHERE movil = '$movil'";
+
+    if (mysqli_query($conn, $sql_3)) {
+        echo "Registro actualizado correctamente";
+    } else {
+        echo "Error al actualizar el registro: " . mysqli_error($conn);
+    }
+
+    ?>
+    <div class="grid">
+        <div>
+            <ul>
+                <li>Saldo en voucher: <?php
+                                        // echo "$" . $pago_en_papel;
+                                        ?></li>
+                <li>Saldo en Efectivo: </li>
+                <li>Debe de semanas: <?php //echo $debe_semanas 
+                                        ?> </li>
+            </ul>
+        </div>
+        <div>
+            <ul>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+    </div>
 </body>
 
 </html>
